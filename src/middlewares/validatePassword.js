79c17676f6d3c5c66debb0cc9b19d90e5f883c79
hamomgs <hamomgomer947@gmail.com>
2023-09-14 20:17:23 -0300

    feat(middleware): Completed `validadePassword.js`

--- src/middlewares/validatePassword.js
+++ src/middlewares/validatePassword.js
@@ -1,8 +1,16 @@
 const senha = 'Cubos123Bank'
 
 const validatePassword = (req, res, next) => {
-  const { senha_banco } = req.params
+  const { senha_banco } = req.query
 
+  if (!senha_banco) {
+    return res.status(401).json({ mensagem: 'A senha do banco não foi informada!' })
+  }
+
+  if (senha_banco !== senha) {
+    return res.json({ mensagem: 'A senha do banco informada é inválida!' })
+  }
+  
   next()
 }
 
