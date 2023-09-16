a6dfb659aa33d6b5b47eab323684c9c0b419a54c
hamomgs <hamomgomer947@gmail.com>
2023-09-15 21:33:44 -0300

    feat(controllers): Function `transfer()`

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -97,7 +97,62 @@ const withdraw = (req, res) => {
 }
 
 const transfer = (req, res) => {
+  const transfers = database.transferencias
+  const requiredFields = ['numero_conta_origem', 'numero_conta_destino', 'valor', 'senha']
+  
+  for (const field of requiredFields) {
+    if (!req.body[field]) {
+      return res.status(400).json({ mensagem: `O número da conta de origem e destino, o valor e a senha são obrigatórios!` })
+    }
+  }
+
   const { numero_conta_origem, numero_conta_destino, valor, senha } = req.body
+
+  validOriginAccount = accounts.find((account) => {
+    return account.numero === numero_conta_origem
+  })
+
+  validTargetAccount = accounts.find((account) => {
+    return account.numero === numero_conta_destino
+  })
+
+  if (!validOriginAccount || !validTargetAccount) {
+    return res.status(404).json({ mensagem: 'O número da conta informado não é válido.' })
+  }
+
+  const accountPassword = validOriginAccount.usuario.senha
+
+  if (senha !== accountPassword) {
+    return res.status(400).json({ mensagem: 'A senha informada está incorreta.' })
+  }
+
+  const accountBalance = validOriginAccount.saldo
+
+  if (accountBalance < valor) {
+    return res.status(400).json({ mensagem: 'Saldo insuficiente.' })
+  }
+
+  if (valor < 0) {
+    return res.status(400).json({ mensagem: 'O valor não pode ser menor que zero!' })
+  }
+
+  if (isNaN(valor)) {
+    return res.status(400).json({ mensagem: 'O valor informado não é válido.' })
+  }
+
+  validOriginAccount.saldo -= Number(valor)
+  validTargetAccount.saldo += Number(valor)
+
+  const transferLog = {
+    data: format(new Date(), "dd-MM-yyyy HH:mm:ss"),
+    numero_conta_origem,
+    numero_conta_destino,
+    valor: Number(valor)
+  }
+
+  transfers.push(transferLog)
+
+  return res.status(204).send()
 }
 
 module.exports = {
